2ba9028c4423bafafcd35c99942c3458c7e036ac
erniehs <ernie_hs@hotmail.com>
2020-06-10 19:10:34 +0200

    create things with shift key

--- index.js
+++ index.js
@@ -15,32 +15,78 @@ window.addEventListener("resize", () => {
     resize()
 })
 
+ctx.canvas.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+})
+
 var selected = []
-var md = false
-var mo = [0, 0]
+var md = false, shift = false
+var mo = [0, 0], mp = [0, 0]
+var newThing = null
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "Shift":
+            if (!shift) {
+                newThing = new Thing([mp[0], mp[1]], 0, zerov, 0, com([[mp[0], mp[1]]]))
+                shift = true
+            }
+            break
+    }
+})
+
+window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+        case "Shift":
+            if (shift) {
+                if (newThing.tp.length > 2) {
+                    newThing.p = com(newThing.tp)
+                    newThing.o = centroid(newThing.tp)
+                    things.push(newThing)
+                }
+                newThing = null
+            }
+            shift = false
+            break
+    }
+})
+
+var newThing
 
 window.addEventListener("mousedown", (e) => {
-    if (!md) {
-        mo = [e.clientX, e.clientY]
-        selected = things.filter(t => pinbox(mo, t.bb))
-        md = true
+    mp = [e.clientX, e.clientY]
+    if (shift) {
+        newThing.tp.push([mp[0], mp[1]])
+        newThing.bb = bbox1(newThing.tp)
+    } else {
+        if (!md) {
+            mo = [e.clientX, e.clientY]
+            selected = things.filter(t => pinbox(mp, t.bb))
+            md = true
+        }
     }
 })
 
 window.addEventListener("mouseup", (e) => {
-    if (md) {
-        mo = [e.clientX, e.clientY]
-        selected = []
-        md = false
+    mp = [e.clientX, e.clientY]
+    if (shift) {
+    } else {
+        if (md) {
+            selected = []
+            md = false
+        }
     }
 })
 
 window.addEventListener("mousemove", (e) => {
-    if (md) {
-        mp = [e.clientX, e.clientY]
-        dm = sub(mp, mo)
-        selected.forEach(t => t.o = add(t.o, dm))
-        mo = mp
+    mp = [e.clientX, e.clientY]
+    if (shift) {
+    } else {
+        if (md) {
+            dm = sub(mp, mo)
+            selected.forEach(t => t.o = add(t.o, dm))
+            mo = mp
+        }
     }
 })
 
@@ -50,18 +96,50 @@ var things = [
     new Thing([210, 150], 0.0, zerov, 0, com([[-100, 50], [100, -70], [150, 100], [-100, 200]]))
 ]
 
+const drawThing = (t) => {
+    ctx.strokeStyle = "white"
+    ctx.beginPath()
+    ctx.moveTo(t.tp[0][0], t.tp[0][1])
+    for (let i = 1; i < t.tp.length; i++) ctx.lineTo(t.tp[i][0], t.tp[i][1])
+    ctx.lineTo(t.tp[0][0], t.tp[0][1])
+    ctx.stroke()
+
+    ctx.strokeStyle = "yellow"
+    ctx.strokeRect(t.bb[0][0], t.bb[0][1], t.bb[1][0] - t.bb[0][0], t.bb[1][1] - t.bb[0][1])
+
+    ctx.strokeStyle = "green"
+    ctx.beginPath()
+    ctx.moveTo(t.o[0], t.o[1] - 10)
+    ctx.lineTo(t.o[0], t.o[1] + 10)
+    ctx.moveTo(t.o[0] - 10, t.o[1])
+    ctx.lineTo(t.o[0] + 10, t.o[1])
+    ctx.stroke()
+}
+
 const draw = () => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    things.forEach(t => {
+
+    things.forEach(t => drawThing(t))
+
+    if (newThing != null) {
         ctx.strokeStyle = "white"
         ctx.beginPath()
-        ctx.moveTo(t.tp[0][0], t.tp[0][1])
-        for (let i = 1; i < t.p.length; i++) ctx.lineTo(t.tp[i][0], t.tp[i][1])
-        ctx.lineTo(t.tp[0][0], t.tp[0][1])
+        ctx.moveTo(newThing.tp[0][0], newThing.tp[0][1])
+        for (let i = 1; i < newThing.tp.length; i++) ctx.lineTo(newThing.tp[i][0], newThing.tp[i][1])
+        ctx.lineTo(mp[0], mp[1])
         ctx.stroke()
+
         ctx.strokeStyle = "yellow"
-        ctx.strokeRect(t.bb[0][0], t.bb[0][1], t.bb[1][0] - t.bb[0][0], t.bb[1][1] - t.bb[0][1])
-    })
+        ctx.strokeRect(newThing.bb[0][0], newThing.bb[0][1], newThing.bb[1][0] - newThing.bb[0][0], newThing.bb[1][1] - newThing.bb[0][1])
+
+        ctx.strokeStyle = "green"
+        ctx.beginPath()
+        ctx.moveTo(newThing.o[0], newThing.o[1] - 10)
+        ctx.lineTo(newThing.o[0], newThing.o[1] + 10)
+        ctx.moveTo(newThing.o[0] - 10, newThing.o[1])
+        ctx.lineTo(newThing.o[0] + 10, newThing.o[1])
+        ctx.stroke()
+    }
 }
 
 const update = (dt) => {
