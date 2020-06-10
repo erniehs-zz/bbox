55d1ac792ce301297662d0b10c1b284d166061a1
erniehs <ernie_hs@hotmail.com>
2020-06-10 13:10:01 +0200

    curious for vs reduce

--- bbox.js
+++ bbox.js
@@ -1,4 +1,4 @@
-const bbox = (p) => {
+const bbox1 = (p) => {
     var bb = [[p[0][0], p[0][1]], [p[p.length - 1][0], p[p.length - 1][1]]]
     for (let i = 0; i < p.length; i++) {
         if (p[i][0] < bb[0][0]) bb[0][0] = p[i][0]
@@ -9,25 +9,28 @@ const bbox = (p) => {
     return bb
 }
 
-class Thing {
+const bbox2 = (p) => p.reduce((p, c) => [[Math.min(c[0], p[0][0]), Math.min(c[1], p[0][1])], [Math.max(c[0], p[1][0]), Math.max(c[1], p[1][1])]], [[Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER], [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER]])
 
-    // origin, angle, polygon
-    constructor(o, a, v, w, p) {
-        this.o = o
-        this.a = a
-        this.v = v
-        this.w = w
-        this.p = p
-        this.update(0)
-    }
+// which is quicker?
+const rand = (max) => Math.floor(Math.random() * Math.floor(max))
 
-    update(dt) {
-        this.o = add(this.o, mul(this.v, dt))
-        this.a += this.w * dt
-        let t = affineMatMul(affineMatTrans(this.o), affineMatRot(this.a))
-        this.tp = this.p.map(v => affineMatVecMul(t, v)).map(r => round(r))
-        this.bb = bbox(this.tp)
+const rubbish_shuffle = (a, c = 1 + rand(a.length * 2)) => {
+    var arry = a.slice(0)
+    for(let i = 0; i < c; i++) {
+        let r = rand(a.length)
+        arry = arry.splice(r).concat(arry.splice(0, r)) 
     }
+    return arry
 }
 
+// unless mistaken bbox1 is quicker by a little bit...  for a consistent shuffle count for same size arry...
+const bbox_speed_test = (p, i) => {
 
+    console.time("bbox1")
+    for(let c = 0; c < i; c++) bbox1(rubbish_shuffle(p, 10))
+    console.timeEnd("bbox1")
+
+    console.time("bbox2")
+    for(let c = 0; c < i; c++) bbox2(rubbish_shuffle(p, 10))
+    console.timeEnd("bbox2")
+}
---
+++ thing.js
@@ -0,0 +1,20 @@
+class Thing {
+
+    // origin, angle, polygon
+    constructor(o, a, v, w, p) {
+        this.o = o
+        this.a = a
+        this.v = v
+        this.w = w
+        this.p = p
+        this.update(0)
+    }
+
+    update(dt) {
+        this.o = add(this.o, mul(this.v, dt))
+        this.a += this.w * dt
+        let t = affineMatMul(affineMatTrans(this.o), affineMatRot(this.a))
+        this.tp = this.p.map(v => affineMatVecMul(t, v)).map(r => round(r))
+        this.bb = bbox2(this.tp)
+    }
+}
